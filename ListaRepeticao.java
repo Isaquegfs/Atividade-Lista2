549f9a8f113e5872623e8d29586f86d47a330b5c
Isaque Gabriel <isaquegabriel8@hotmail.com>
2025-05-20 12:11:46 -0300

    Menu das atividades

--- ListaRepeticao.java
+++ ListaRepeticao.java
@@ -1,8 +1,29 @@
 import java.util.Scanner;
 
-public class Lista {
+public class ListaRepeticao {
     public static void main(String[] args) throws Exception {
-        // Scanner entrada=new Scanner(System.in);
-        System.out.println("Ola");
+        Scanner entrada=new Scanner(System.in);
+
+        // Menu
+        System.out.println("+===========================+");
+        System.out.println("       Atividade Lista 2     ");
+        System.out.println("     LISTA DE EXERCICIOS     ");
+        System.out.println("+===========================+");
+        System.out.println("1 - Exibe 30 vezes");
+        System.out.println("2 - Incrementa 1000");
+        System.out.println("3 - Inteiros 100 a 1");
+        System.out.println("4 - Media Vida Radioatividade");
+        System.out.println("5 - Posto de Gasolina");
+        System.out.println("6 - Media Aritmética");
+        System.out.println("7 - Verifica Primo");
+        System.out.println("8 - Soma Intervalo");
+        System.out.println("9 - Divisores N");
+        System.out.println("10 - Adivinha Numero");
+        System.out.println("11 - População Bruzundangas");
+        System.out.println("12 - Média População");
+
+
+
+        
     }
 }
