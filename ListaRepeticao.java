51cca683b76ebf90e8321334f12b53bedb438b21
Isaque Gabriel <isaquegabriel8@hotmail.com>
2025-05-20 12:37:49 -0300

    Menu de selecionar atividade, e loop de repetição

--- ListaRepeticao.java
+++ ListaRepeticao.java
@@ -23,7 +23,63 @@ public class ListaRepeticao {
         System.out.println("12 - Média População");
 
 
+        // Repetir o aplicativo
+        int encerrar = 1;
+        while (encerrar != 0) {
 
+            // Selecionar questão
+            int selecionarQuestao;
+            while (true) {
+                System.out.print("Qual atividade gostaria de executar: ");
+                selecionarQuestao = entrada.nextInt();
+                if (selecionarQuestao < 1 || selecionarQuestao > 12) {
+                    System.out.println("Número inválido! Digite uma opção de 1 e 12");
+                }
+                else {
+                    break;
+                }
+            }
+
+            // Atividade Selecionada
+            System.out.println("Exercício " + selecionarQuestao + " selecionado");
+            System.out.println("Enunciado: ");
+            
+            // Atividades
+            switch(selecionarQuestao){
+                case 1:{}
+                case 2:{}
+                case 3:{}
+                case 4:{}
+                case 5:{}
+                case 6:{}
+                case 7:{}
+                case 8:{}
+                case 9:{}
+                case 10:{}
+                case 11:{}
+                case 12:{}
+
+
+
+            
+            // Chave switch
+            }
         
+        // Chave da repetição do aplicativo
+        }
+
+
+
+
+
+
+
+
+
+
+
+        entrada.close();
+    // Chave static void
     }
+// Chave classe
 }
