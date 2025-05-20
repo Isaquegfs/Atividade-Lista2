509e44b6c4f224fdde16f133d2cd4d42684947db
Isaque Gabriel <isaquegabriel8@hotmail.com>
2025-05-20 12:09:43 -0300

    Inicio lista de atividades 2

---
+++ ListaRepeticao.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Lista {
+    public static void main(String[] args) throws Exception {
+        // Scanner entrada=new Scanner(System.in);
+        System.out.println("Ola");
+    }
+}
